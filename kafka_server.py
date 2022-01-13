d0050bd94161c103dda01e6a25ea060d7fa27fef
Ola Ajibode <ola.ajibode@gmail.com>
2022-01-13 17:22:59 +0000

    Feat: Generate data from json via kafka_server

--- kafka_server.py
+++ kafka_server.py
@@ -8,20 +8,18 @@ def run_kafka_server():
     # TODO get the json file path
     input_file = "data/police-department-calls-for-service.json"
 
-    # TODO fill in blanks
+    # TODO fill in blanks - Done
     producer = producer_server.ProducerServer(
         input_file=input_file,
         topic=KAFKA_TOPIC,
         bootstrap_servers=BOOTSTRAP_SERVER,
         client_id=f"producer.{KAFKA_TOPIC}",
     )
-
     return producer
 
 
 def feed():
     producer = run_kafka_server()
-    print("Generating data in a second")
     producer.generate_data()
 
 
--- producer_server.py
+++ producer_server.py
@@ -1,13 +1,14 @@
-from kafka import KafkaProducer
+from kafka.producer import KafkaProducer
 import json
 import time
 
 
 class ProducerServer(KafkaProducer):
-    """Producve Kafka messages to a topic from the JSON file
-        :param input_file:  JSON file for source of events(calls)
-        :param topic: Kafka topic to write events to
+    """Produce Kafka messages to a topic from the JSON file
+    :param input_file:  JSON file for source of events(calls)
+    :param topic: Kafka topic to write events to
     """
+
     def __init__(self, input_file, topic, **kwargs):
         super().__init__(**kwargs)
         self.input_file = input_file
@@ -16,12 +17,14 @@ class ProducerServer(KafkaProducer):
     # TODO we're generating a dummy data - Done
     def generate_data(self):
         with open(self.input_file) as f:
-            for line in f:
+            json_data = json.load(f)
+            for line in json_data:
                 message = self.dict_to_binary(line)
-                # TODO send the correct data
+                # TODO send the correct data - Done
                 self.send(topic=self.topic, value=message)
                 time.sleep(1)
 
     # TODO fill this in to return the json dictionary to binary - Done
-    def dict_to_binary(self, json_dict):
+    @staticmethod
+    def dict_to_binary(json_dict):
         return json.dumps(json_dict).encode("utf-8")
