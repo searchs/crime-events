c77e5d85b72ec183a6fcb3054453da99b8756b50
Ola Ajibode <ola.ajibode@gmail.com>
2022-01-21 00:27:15 +0000

    Feat: Add screenshot to README

--- kafka_server.py
+++ kafka_server.py
@@ -1,20 +1,27 @@
+import logging
+
 import producer_server
 
+
 BOOTSTRAP_SERVER = "localhost:9092"
-KAFKA_TOPIC = "sf.crime.calls"
+KAFKA_TOPIC = "crimes.calls"
+CLIENT_ID = f"producer.{KAFKA_TOPIC}"
+
+logger = logging.getLogger(__name__)
 
 
 def run_kafka_server():
-    # TODO get the json file path
-    input_file = "data/police-department-calls-for-service.json"
+    # TODO get the json file path - Done
+    input_file = "police-department-calls-for-service.json"
 
-    # TODO fill in blanks - Done
+    # TODO fill in blanks
     producer = producer_server.ProducerServer(
         input_file=input_file,
         topic=KAFKA_TOPIC,
         bootstrap_servers=BOOTSTRAP_SERVER,
-        client_id=f"producer.{KAFKA_TOPIC}",
+        client_id=CLIENT_ID,
     )
+
     return producer
 
 
--- runner.sh
+++ runner.sh
@@ -23,8 +23,8 @@ bin/kafka-console-consumer.sh --bootstrap-server localhost:9092  --topic sf.crim
 
 
 #Create topic manually
-/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 3 --partitions 2   --topic sf.crime.calls
-
+/usr/bin/kafka-topics.sh --create  --partitions 1 --replication-factor 1    --topic sf.crime.calls  --bootstrap-server localhost:9092
+$ bin/kafka-topics.sh --create --partitions 1 --replication-factor 1 --topic quickstart-events --bootstrap-server localhost:9092
 #Topics list details
 /bin/kafka-topics --zookeeper localhost:2181 --list
 
@@ -41,3 +41,46 @@ bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic sf.crime
 
 spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.4 --master local[*] data_stream.py
 
+# ZooKeeper operations
+bin/zookeeper-server-start.sh config/zookeeper.properties
+bin/kafka-server-start.sh config/server.properties
+
+
+/usr/bin/zookeeper-server-start zookeeper.properties
+/usr/bin/kafka-server-start producer.properties
+
+
+# Runner
+/usr/bin/zookeeper-server-start config/zookeeper.properties
+/usr/bin/kafka-server-start config/server.properties
+
+touch consumer_server.py
+
+# Analytics: Run data_stream [update spark.conf with the spark-sql-kafka ]
+spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.4 --master local[*] data_stream.py
+
+# Manually create topic
+kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 2 --topic crimes.calls
+
+# On Workspace
+bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic sf.crime.calls --from-beginning --max-message 10
+
+# Consume
+/usr/bin/kafka-consumer-console --bootstrap-server localhost:9092 --topic crime.calls --from-beginning
+
+
+# Test Kafka server
+bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic <your-topic-name> --from-beginning
+
+/usr/bin/kafka-console-consumer --bootstrap-server localhost:9092 --topic sfrancisco.crime.calls --from-beginning
+/usr/bin/kafka-console-consumer --bootstrap-server localhost:9092 --topic sfrancisco.crime.calls --from-beginning  --max-messages 10
+
+
+spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.4 --master local[*] data_stream.py
+
+
+# Check topic info
+/usr/bin/kafka-topics --zookeeper localhost:2181 --topic sf.crime.calls --describe
+
+# Delete topic
+/usr/bin/kafka-topics --zookeeper localhost:2181 --topic sf.crime.calls --delete
