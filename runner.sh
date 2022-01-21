847de2fdb952ec99be7cc675e0fe94a7279bc097
Ola Ajibode <ola.ajibode@gmail.com>
2022-01-21 01:09:12 +0000

    Feat: Cleanup runner script

--- runner.sh
+++ runner.sh
@@ -2,85 +2,27 @@
 # Setup Environment
 sh startup.sh
 
-
-# ZooKeeper operations
-bin/zookeeper-server-start.sh config/zookeeper.properties
-bin/kafka-server-start.sh config/server.properties
-
-
-/usr/bin/zookeeper-server-start zookeeper.properties
-/usr/bin/kafka-server-start producer.properties
-
-
-# Runner
+# Run ZooKeeper using properties in config directory
 /usr/bin/zookeeper-server-start config/zookeeper.properties
+
+# Run Kafka server  using properties in config directory
 /usr/bin/kafka-server-start config/server.properties
 
-touch consumer_server.py
+#Create Kafka topic manually
+/usr/bin/kafka-topics --create  --partitions 1 --replication-factor 1 --topic <topic name>  --bootstrap-server localhost:<port>
+/usr/bin/kafka-topics --create  --partitions 1 --replication-factor 1 --topic crimes.calls  --bootstrap-server localhost:9092
 
-# On Workspace
-bin/kafka-console-consumer.sh --bootstrap-server localhost:9092  --topic sf.crime.calls --from-beginning
+# Check list of existing Kafka topics
+/usr/bin/kafka-topics --zookeeper localhost:2181 --list
 
-
-#Create topic manually
-/usr/bin/kafka-topics.sh --create  --partitions 1 --replication-factor 1    --topic sf.crime.calls  --bootstrap-server localhost:9092
-$ bin/kafka-topics.sh --create --partitions 1 --replication-factor 1 --topic quickstart-events --bootstrap-server localhost:9092
-#Topics list details
-/bin/kafka-topics --zookeeper localhost:2181 --list
-
-
-$ bin/kafka-console-consumer.sh --topic sf.crime.calls quickstart-events --from-beginning --bootstrap-server localhost:9092
-
-# Consume
-#Test pub
-/usr/bin/kafka-console-consumer --bootstrap-server localhost:9092 --topic sf.crime.calls --from-beginning
+# Consume messages from a topic --optional parameter: --max-message 10
+/usr/bin/kafka-console-consumer --bootstrap-server localhost:9092 --topic crimes.calls --from-beginning
 
 # Test Kafka server
 bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic sf.crime.calls --from-beginning
 
-
+#Run transformation and analytics on consumed messages
 spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.4 --master local[*] data_stream.py
-
-# ZooKeeper operations
-bin/zookeeper-server-start.sh config/zookeeper.properties
-bin/kafka-server-start.sh config/server.properties
-
-
-/usr/bin/zookeeper-server-start zookeeper.properties
-/usr/bin/kafka-server-start producer.properties
-
-
-# Runner
-/usr/bin/zookeeper-server-start config/zookeeper.properties
-/usr/bin/kafka-server-start config/server.properties
-
-touch consumer_server.py
-
-# Analytics: Run data_stream [update spark.conf with the spark-sql-kafka ]
-spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.4 --master local[*] data_stream.py
-
-# Manually create topic
-kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 2 --topic crimes.calls
-
-# On Workspace
-bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic sf.crime.calls --from-beginning --max-message 10
-
-# Consume
-/usr/bin/kafka-consumer-console --bootstrap-server localhost:9092 --topic crime.calls --from-beginning
-
-
-# Test Kafka server
-bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic <your-topic-name> --from-beginning
-
-/usr/bin/kafka-console-consumer --bootstrap-server localhost:9092 --topic sfrancisco.crime.calls --from-beginning
-/usr/bin/kafka-console-consumer --bootstrap-server localhost:9092 --topic sfrancisco.crime.calls --from-beginning  --max-messages 10
-
-
-spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.4 --master local[*] data_stream.py
-
 
 # Check topic info
-/usr/bin/kafka-topics --zookeeper localhost:2181 --topic sf.crime.calls --describe
-
-# Delete topic
-/usr/bin/kafka-topics --zookeeper localhost:2181 --topic sf.crime.calls --delete
+/usr/bin/kafka-topics --zookeeper localhost:2181 --topic crimes.calls --describe
